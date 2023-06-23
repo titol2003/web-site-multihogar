ec1a088bc41fc40e8cb615d238bfd40f9c813c5e
titol2003 <titolospina@gmail.com>
2023-06-22 19:47:25 -0500

    Inicio de Proyecto

--- src/components/Nosotros.js
+++ src/components/Nosotros.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import './Nosotros.css'
 
+
 import { Link } from 'react-router-dom'
 
-import Pod from '../assets/homii.jpg'
-import Pad from '../assets/salii.jpg'
+import Pod from '../assets/ImagesHome/homii.jpg'
+import Pad from '../assets/ImagesHome/salii.jpg'
+
+
 
 const Nosotros = () => {
   return (
     <div className='nosotros'>
+        
         <div className='left'>
             <h1>Nosotros</h1>
             <p>Consigue la casa de tus sueños!</p>
             <Link to='/registro'><button className='btn'>REGISTRARSE</button></Link>
         </div>
+        
         <div className='right'>
             <div className='img-container'>
                 <div className='image-stack top'>
@@ -22,9 +27,11 @@ const Nosotros = () => {
                 <div className='image-stack bottom'>
                     <img src={Pad} className='img' alt='' />
                 </div>
-            </div>
+            </div>  
         </div>
+        
     </div>
+    
   )
 }
 
