e2b1bd27a4f710e9aeb0706d3b2d9909319ae21c
titol2003 <titolospina@gmail.com>
2023-07-25 22:16:34 -0500

    Remodelaciones y ajustes pequeños (path y elimiar archivos sin utilidad)

--- src/pages/Publicar.js
+++ src/pages/Publicar.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import PublicarInmueble from '../components/PublicarInmueble/PublicarInmueble'
+import AgenteNavbar from '../components/AgenteInmueble/AgenteNavbar'
 
 const Publicar = () => {
     return (
-        <div>            
+        <div> 
+            <AgenteNavbar />
+            <br />
+            <br />
             <PublicarInmueble />            
         </div>
     )
